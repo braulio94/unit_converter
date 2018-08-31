32fdefe70beb1899e19674063e35d00b8e2c565b
braulio94 <brauliocassule94@gmail.com>
2018-09-01 00:44:25 +0100

    add Category Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,62 @@ class UnitConverterApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
-      home: Category(),
+      home: Scaffold(
+        backgroundColor: Colors.green[100],
+        body: Center(
+          child: Category(
+            name: 'Cake',
+            color: Colors.green,
+            iconLocation: Icons.cake,
+          ),
+        ),
+      ),
     );
   }
 }
 
 class Category extends StatelessWidget {
+
+  final String name;
+  final Color color;
+  final IconData iconLocation;
+
+  Category({this.name, this.color, this.iconLocation});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final _rowHeight = 100.0;
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(_rowHeight/2),
+          highlightColor: color,
+          splashColor: color,
+          onTap: (){
+
+          },
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(name, style: Theme.of(context).textTheme.headline, textAlign: TextAlign.center),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

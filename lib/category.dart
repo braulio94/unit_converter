4e6ed462a5462a56ec6bd910afc1a244a15f6333
braulio94 <brauliocassule94@gmail.com>
2018-09-01 04:26:15 +0100

    create Category model

--- lib/category.dart
+++ lib/category.dart
@@ -1,70 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:unit_converter/converter_route.dart';
 import 'package:unit_converter/unit.dart';
 
-class Category extends StatelessWidget {
+
+class Category {
 
   final String name;
-  final Color color;
-  final IconData iconLocation;
+  final ColorSwatch color;
   final List<Unit> units;
+  final IconData iconLocation;
 
-  Category({this.name, this.color, this.iconLocation, this.units});
-
-  void _navigateToConverter(BuildContext context){
-    Navigator.of(context).push(MaterialPageRoute<Null>(
-      builder: (context){
-        return Scaffold(
-          appBar: AppBar(
-            elevation: 1.0,
-            title: Text(name, style: Theme.of(context).textTheme.display1),
-            centerTitle: true,
-            backgroundColor: color,
-          ),
-          body: ConverterRoute(
-            name: name,
-            color: color,
-            units: units,
-          ),
-        );
-      }
-    ));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final _rowHeight = 100.0;
-    return Material(
-      color: Colors.transparent,
-      child: Container(
-        height: _rowHeight,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(_rowHeight/2),
-          highlightColor: color,
-          splashColor: color,
-          onTap: (){
-            _navigateToConverter(context);
-          },
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Icon(
-                    iconLocation,
-                    size: 60.0,
-                  ),
-                ),
-                Center(
-                  child: Text(name, style: Theme.of(context).textTheme.headline, textAlign: TextAlign.center),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+  Category({this.name, this.color, this.units, this.iconLocation});
 }

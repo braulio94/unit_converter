b5f6bd6b073632ec8405347907e17bdc8888f64e
braulio94 <brauliocassule94@gmail.com>
2018-09-01 01:33:28 +0100

    edited Category file

--- lib/category.dart
+++ lib/category.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:unit_converter/converter_route.dart';
+import 'package:unit_converter/unit.dart';
 
 class Category extends StatelessWidget {
 
   final String name;
   final Color color;
   final IconData iconLocation;
+  final List<Unit> units;
 
-  Category({this.name, this.color, this.iconLocation});
+  Category({this.name, this.color, this.iconLocation, this.units});
+
+  void _navigateToConverter(BuildContext context){
+    Navigator.of(context).push(MaterialPageRoute<Null>(
+      builder: (context){
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 1.0,
+            title: Text(name, style: Theme.of(context).textTheme.display1),
+            centerTitle: true,
+            backgroundColor: color,
+          ),
+          body: ConverterRoute(
+            name: name,
+            color: color,
+            units: units,
+          ),
+        );
+      }
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +43,7 @@ class Category extends StatelessWidget {
           highlightColor: color,
           splashColor: color,
           onTap: (){
-
+            _navigateToConverter(context);
           },
           child: Padding(
             padding: EdgeInsets.all(8.0),

768bbec7e9b5d96a4369ee315bd14013a68dd647
braulio94 <brauliocassule94@gmail.com>
2018-09-01 03:26:40 +0100

    ConverterRoute makes conversions

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -17,7 +17,7 @@ class _ConverterRouteState extends State<ConverterRoute> {
   Unit _fromValue;
   Unit _toValue;
   double _inputValue;
-  String _convertedValue;
+  String _convertedValue = '';
   List<DropdownMenuItem> _unitMenuItems;
   bool _showValidationError = false;
 
@@ -50,6 +50,75 @@ class _ConverterRouteState extends State<ConverterRoute> {
       _fromValue = widget.units[0];
       _toValue = widget.units[1];
     });
+  }
+
+  void _updateConversion() {
+    setState(() {
+      _convertedValue =
+          _format(_inputValue * (_toValue.conversion / _fromValue.conversion));
+    });
+  }
+
+  void _updateInputValue(String input) {
+    setState(() {
+      if (input == null || input.isEmpty) {
+        _convertedValue = '';
+      } else {
+        // Even though we are using the numerical keyboard, we still have to check
+        // for non-numerical input such as '5..0' or '6 -3'
+        try {
+          final inputDouble = double.parse(input);
+          _showValidationError = false;
+          _inputValue = inputDouble;
+          _updateConversion();
+        } on Exception catch (e) {
+          print('Error: $e');
+          _showValidationError = true;
+        }
+      }
+    });
+  }
+
+  void _updateFromConversion(dynamic unitName) {
+    setState(() {
+      _fromValue = _getUnit(unitName);
+    });
+    if(_inputValue != null){
+      _updateConversion();
+    }
+  }
+
+  void _updateToConversion(dynamic unitName) {
+    setState(() {
+      _toValue = _getUnit(unitName);
+    });
+    if (_inputValue != null) {
+      _updateConversion();
+    }
+  }
+
+  Unit _getUnit(String unitName){
+    return widget.units.firstWhere(
+        (Unit unit){
+          return unit.name == unitName;
+        },
+      orElse: null,
+    );
+  }
+
+  String _format(double conversion) {
+    var outputNum = conversion.toStringAsPrecision(7);
+    if (outputNum.contains('.') && outputNum.endsWith('0')) {
+      var i = outputNum.length - 1;
+      while (outputNum[i] == '0') {
+        i -= 1;
+      }
+      outputNum = outputNum.substring(0, i + 1);
+    }
+    if (outputNum.endsWith('.')) {
+      return outputNum.substring(0, outputNum.length - 1);
+    }
+    return outputNum;
   }
 
   _createDropdown({String currentValue, ValueChanged<dynamic> onChanged}){
@@ -97,7 +166,7 @@ class _ConverterRouteState extends State<ConverterRoute> {
             keyboardType: TextInputType.number,
             onChanged: _updateInputValue,
           ),
-          _createDropdown(currentValue: _fromValue.name)//, onChanged: _updateFromConversion)
+          _createDropdown(currentValue: _fromValue.name, onChanged: _updateFromConversion)
         ],
       ),
     );
@@ -107,18 +176,28 @@ class _ConverterRouteState extends State<ConverterRoute> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           InputDecorator(
-            //TODO put converted value here
-            child: Text('', style: Theme.of(context).textTheme.display1,),
+            child: Text(
+              _convertedValue,
+              style: Theme.of(context).textTheme.display1,
+            ),
             decoration: InputDecoration(
-                labelStyle: Theme.of(context).textTheme.display1,
-                labelText: 'Output',
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(0.0),
-                ),
+              labelText: 'Output',
+              labelStyle: Theme.of(context).textTheme.display1,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(0.0),
+              ),
             ),
           ),
-          _createDropdown(currentValue: _toValue.name)//, onChanged: _updateFromConversion)
+          _createDropdown(currentValue: _toValue.name, onChanged: _updateToConversion),
         ],
+      ),
+    );
+
+    final arrows = RotatedBox(
+      quarterTurns: 1,
+      child: Icon(
+        Icons.compare_arrows,
+        size: 40.0,
       ),
     );
     return Padding(
@@ -127,6 +206,7 @@ class _ConverterRouteState extends State<ConverterRoute> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           input,
+          arrows,
           output,
         ],
       ),

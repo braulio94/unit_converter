5c694c87796183ab67a6b53d9a369415c6c3e48d
braulio94 <brauliocassule94@gmail.com>
2018-09-01 01:17:29 +0100

    add ConverterRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unit_converter/category_route.dart';
+import 'package:unit_converter/unit.dart';
 
 void main() => runApp(UnitConverterApp());
 
@@ -11,6 +12,33 @@ class UnitConverterApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Unit Converter',
       home: CategoryRoute(),
+    );
+  }
+}
+
+class ConverterRoute extends StatelessWidget {
+  final name;
+  final Color color;
+  final List<Unit> units;
+
+  ConverterRoute({this.name, this.color, this.units});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: units.map((Unit unit){
+        return Container(
+          color: color,
+          margin: EdgeInsets.all(8.0),
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Text(unit.name, style: Theme.of(context).textTheme.headline),
+              Text('Conversion: ${unit.conversion}', style: Theme.of(context).textTheme.subhead)
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
